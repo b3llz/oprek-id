53f7ab8c90833472dd9da81a1f5768ccf44c41eb
B3LLZ <160164765+b3llz@users.noreply.github.com>
2026-07-09 10:45:29 +0700

    Update api_keys.dart

--- lib/core/constants/api_keys.dart
+++ lib/core/constants/api_keys.dart
@@ -1,1 +1,8 @@
+class ApiKeys {
+  // TODO: Nanti kamu harus daftar di aistudio.google.com untuk dapat key gratis
+  // Untuk sementara biarkan kosong atau isi dengan key milikmu.
+  // JANGAN PERNAH pamer key ini ke publik/sosmed ya bosku!
+  static const String geminiApiKey = 'ISI_DENGAN_API_KEY_GEMINI_KAMU';
+}
+
 
