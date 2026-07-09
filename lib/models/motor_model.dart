161691af37144cb2153ac7e0c93d67dd179158db
B3LLZ <160164765+b3llz@users.noreply.github.com>
2026-07-09 14:12:21 +0700

    Update motor_model.dart

--- lib/models/motor_model.dart
+++ lib/models/motor_model.dart
@@ -2,13 +2,17 @@ class MotorModel {
   final String id;
   final String brand;
   final String name;
-  final String type;
+  final String type; // Matic, Manual, Kopling
   final double cc;
   final String imagePath;
 
   const MotorModel({
-    required this.id, required this.brand, required this.name,
-    required this.type, required this.cc, required this.imagePath,
+    required this.id,
+    required this.brand,
+    required this.name,
+    required this.type,
+    required this.cc,
+    required this.imagePath,
   });
 }
 
