c7c6bf05582bcc9ecbd6fe7f891a22d7d358339c
B3LLZ <160164765+b3llz@users.noreply.github.com>
2026-07-09 10:11:25 +0700

    Update motor_model.dart

--- lib/models/motor_model.dart
+++ lib/models/motor_model.dart
@@ -1,1 +1,19 @@
+class MotorModel {
+  final String id;
+  final String brand; // Honda, Yamaha, Suzuki, dll
+  final String name; // Beat, NMAX, Aerox, dll
+  final String type; // Matic, Manual, Kopling
+  final double cc; // Kapasitas mesin
+  final String imagePath; // Path gambar dari assets nanti
+
+  const MotorModel({
+    required this.id,
+    required this.brand,
+    required this.name,
+    required this.type,
+    required this.cc,
+    required this.imagePath,
+  });
+}
+
 
